543520a20143a1e528b7a0f8730d4bafbeed1558
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2026-03-22 07:57:53 -0400

    main accounted for servo

--- Arduino Code/main/main.ino
+++ Arduino Code/main/main.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 // Pin Assignments
 const int stepPin = 5;
 const int dirPin = 6;
@@ -5,12 +7,22 @@ const int enPin = 8;
 const int ms1Pin = 2;
 const int ms2Pin = 3;
 const int ms3Pin = 4;
+const int servoPin = 7; 
 
-int stepDelay = 800;
-bool isMoving = false; // Tracks if the motor should be stepping
+Servo myServo;
+bool servoPos = false; 
+
+// --- CONFIGURATION ---
+int stepDelay = 800;      
+int stepsPerPress = 200;  
+// ----------------------
 
 void setup() {
-  Serial.begin(9600); // Start Serial communication
+  Serial.begin(9600);
+
+  myServo.attach(servoPin);
+  // Setting 90 as the "Home" position is usually best for 180-degree movement
+  myServo.write(90); 
 
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -19,35 +31,46 @@ void setup() {
   pinMode(ms2Pin, OUTPUT);
   pinMode(ms3Pin, OUTPUT);
   
-  digitalWrite(enPin, LOW); // Enable driver
-  digitalWrite(ms1Pin, LOW); // Full step
+  digitalWrite(enPin, LOW); 
+  digitalWrite(ms1Pin, LOW); 
   digitalWrite(ms2Pin, LOW);
   digitalWrite(ms3Pin, LOW);
 }
 
-void loop() {
-  // Check if Python sent a command
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-
-    if (command == 'F') {
-      digitalWrite(dirPin, LOW);
-      isMoving = true;
-    } 
-    else if (command == 'B') {
-      digitalWrite(dirPin, HIGH);
-      isMoving = true;
-    } 
-    else if (command == 'S') {
-      isMoving = false;
-    }
-  }
-
-  // If we are in a moving state, take one step
-  if (isMoving) {
+void moveSteps(int steps, int direction) {
+  digitalWrite(dirPin, direction);
+  for (int i = 0; i < steps; i++) {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(stepDelay);
     digitalWrite(stepPin, LOW);
     delayMicroseconds(stepDelay);
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    if (command == 'F') {
+      moveSteps(stepsPerPress, LOW);
+      Serial.println("Done Forward");
+    } 
+    else if (command == 'B') {
+      moveSteps(stepsPerPress, HIGH);
+      Serial.println("Done Backward");
+    }
+    else if (command == 'X') {
+      if (!servoPos) {
+        // Move to 180 degrees (far "right")
+        myServo.write(180);
+        servoPos = true;
+        Serial.println("Servo to 180");
+      } else {
+        // Return to 90 degrees (center)
+        myServo.write(90);
+        servoPos = false;
+        Serial.println("Servo to 90");
+      }
+    }
   }
 }
